3583689e22f86bb6d8b1a0cab426503cf5f2b849
Chase Nicks <chasenicks81@gmail.com>
2021-06-22 19:14:33 -0400

    Declared intial variables and specified character types that will be used to get the ball rolling

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,37 @@
-// Assignment Code
+// genrate botton
 var generateBtn = document.querySelector("#generate");
+var enter;
+// charater's
+var confirmNumber;
+var confirmCharater;
+var confirmUppercase;
+var confirmLowercase;
+
+// Pasword charaters allowed
+number  = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+alphabetical = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+space = [];
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
